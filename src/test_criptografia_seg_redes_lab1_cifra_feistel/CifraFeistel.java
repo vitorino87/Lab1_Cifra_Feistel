2c54806670dd24556c053ec412c884ab267dd7bc
Tiago Vitorino <brother_87_22@hotmail.com>
2018-09-29 18:43:57 -0300

    cifra de feistel melhorado - p/ caracteres iguais, resultados diferentes 29-09-18 18:44

--- src/test_criptografia_seg_redes_lab1_cifra_feistel/CifraFeistel.java
+++ src/test_criptografia_seg_redes_lab1_cifra_feistel/CifraFeistel.java
@@ -5,26 +5,29 @@
  */
 package test_criptografia_seg_redes_lab1_cifra_feistel;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author tiago.lucas
  */
 public class CifraFeistel {
-    public String criptografar(int entrada, int key1, int key2){
+
+    public String criptografar(int entrada, int key1, int key2) {
 ///////////////////////1ª PARTE/////////////////////////////////////////////////        
         //Entrada do bloco de 8 bits
         //Convertendo para binário e depois string
         String bloco = Integer.toBinaryString(entrada);
         bloco = checarTamanho(bloco, 8);
         //quebrando a String em duas partes
-        String parteL = bloco.substring(0, bloco.length()/2);
-        String parteR = bloco.substring(bloco.length()/2, bloco.length());             
+        String parteL = bloco.substring(0, bloco.length() / 2);
+        String parteR = bloco.substring(bloco.length() / 2, bloco.length());
         //convertendo a parte2 para int
         Binario binario = new Binario();
         int L = binario.converterBinarioParaDecimal(parteL);
-        int R = binario.converterBinarioParaDecimal(parteR);        
+        int R = binario.converterBinarioParaDecimal(parteR);
         //Funcao F
-        int funcao = calcularFuncao(R,key1);
+        int funcao = calcularFuncao(R, key1);
         //Realizar Operação XOR
         int xor = L ^ funcao;
         //invertendo os resultados
@@ -40,19 +43,19 @@ public class CifraFeistel {
         //invertendo os resultados
         L = R;
         R = xor;
-        bloco = Integer.toBinaryString(L)+Integer.toBinaryString(R);
+        bloco = Integer.toBinaryString(L) + Integer.toBinaryString(R);
         return bloco;
         //System.out.println("Bloco cifrado: "+bloco);
 ///////////////////////FIM DA 2ª PARTE//////////////////////////////////////////   
     }
-    
-    public String descriptografar(int blocoCripto, int key1, int key2){
+
+    public String descriptografar(int blocoCripto, int key1, int key2) {
 ///////////////1ª PARTE///////////////////////////////////////////////////  
         //preparando e dividindo o bloco em L e R
         String bloco = Integer.toBinaryString(blocoCripto);
-        bloco = checarTamanho(bloco,8);
-        String parteL = bloco.substring(0, bloco.length()/2);
-        String parteR = bloco.substring(bloco.length()/2, bloco.length()); 
+        bloco = checarTamanho(bloco, 8);
+        String parteL = bloco.substring(0, bloco.length() / 2);
+        String parteR = bloco.substring(bloco.length() / 2, bloco.length());
         Binario binario = new Binario();
         //convertendo binário para decimal
         int L = binario.converterBinarioParaDecimal(parteL);
@@ -72,20 +75,131 @@ public class CifraFeistel {
         //invertendo os resultados
         R = L;
         L = xor;
-        return checarTamanho(Integer.toBinaryString(L),4) + checarTamanho(Integer.toBinaryString(R),4);
+        return checarTamanho(Integer.toBinaryString(L), 4) + checarTamanho(Integer.toBinaryString(R), 4);
     }
-    
-    public int calcularFuncao(int val, int key){        
-        return val-1+key+22;
+
+    public int calcularFuncao(int val, int key) {
+        return val - 1 + key + 22;
     }
-    
-    public String checarTamanho(String input, int tam){
-        if(input.length()<tam){
+
+    public String checarTamanho(String input, int tam) {
+        if (input.length() < tam) {
             int num = tam - input.length();
-            for(int i=0;i<num;i++){
-                input = "0"+input;
-            }            
+            for (int i = 0; i < num; i++) {
+                input = "0" + input;
+            }
         }
         return input;
-    }        
+    }
+
+    public String realizarCriptografia(String text, int key1, int key2) {
+        String textCripto = "";
+        for (int i = 0; i < text.length(); i++) {
+            String aux = criptografar(text.charAt(i), key1, key2);
+            Binario b = new Binario();
+            int auxChar = b.converterBinarioParaDecimal(aux);
+            textCripto += (char) auxChar;
+        }
+        return textCripto;
+    }
+
+    public String realizarDescriptografia(String textCripto, int key1, int key2) {
+        String text = "";
+        for (int i = 0; i < textCripto.length(); i++) {
+            String aux = descriptografar(textCripto.charAt(i), key1, key2);
+            Binario b = new Binario();
+            int auxChar = b.converterBinarioParaDecimal(aux);
+            text += (char) auxChar;
+        }
+        return text;
+    }
+
+    public String realizarCriptografia(String text, String key1, String key2) {
+        String textCripto = "";
+        int[] keyAux1 = new int[key1.length()];
+        int[] keyAux2 = new int[key2.length()];
+        try {
+            for (int i = 0; i < key1.length(); i++) {
+                keyAux1[i] = Integer.valueOf(String.valueOf(key1.charAt(i)));
+            }
+            for (int i = 0; i < key2.length(); i++) {
+                keyAux2[i] = Integer.valueOf(String.valueOf(key2.charAt(i)));
+            }
+        } catch (Exception ex) {
+            try{
+                for (int i = 0; i < key1.length(); i++) {
+                    keyAux1[i] = key1.charAt(i);
+                }
+                for (int i = 0; i < key2.length(); i++) {
+                    keyAux2[i] = key2.charAt(i);
+                }
+            }catch(Exception ex1){
+                System.out.println("Chave inválida. Erro: "+ex1.toString());
+            }
+        }
+
+        int cont1 = 0;
+        int cont2 = 0;
+        for (int i = 0; i < text.length(); i++) {
+            String aux = criptografar(text.charAt(i), keyAux1[cont1], keyAux2[cont2]);
+            Binario b = new Binario();
+            int auxChar = b.converterBinarioParaDecimal(aux);
+            textCripto += (char) auxChar;
+            cont1++;
+            cont2++;
+            if (cont1 == key1.length()) {
+                cont1 = 0;
+            }
+            if (cont2 == key2.length()) {
+                cont2 = 0;
+            }
+        }
+        return textCripto;
+    }
+
+    public String realizarDescriptografia(String textCripto, String key1, String key2) {
+        String text = "";
+        int[] keyAux1 = new int[key1.length()];
+        int[] keyAux2 = new int[key2.length()];
+        for (int i = 0; i < key1.length(); i++) {
+            keyAux1[i] = Integer.valueOf(String.valueOf(key1.charAt(i)));
+        }
+        for (int i = 0; i < key2.length(); i++) {
+            keyAux2[i] = Integer.valueOf(String.valueOf(key2.charAt(i)));
+        }
+        int cont1 = 0;
+        int cont2 = 0;
+        for (int i = 0; i < textCripto.length(); i++) {
+            String aux = descriptografar(textCripto.charAt(i), keyAux1[cont1], keyAux2[cont2]);
+            Binario b = new Binario();
+            int auxChar = b.converterBinarioParaDecimal(aux);
+            text += (char) auxChar;
+            cont1++;
+            cont2++;
+            if (cont1 == key1.length()) {
+                cont1 = 0;
+            }
+            if (cont2 == key2.length()) {
+                cont2 = 0;
+            }
+        }
+        return text;
+    }
+
+    public ArrayList<String> dividirBinario(int entrada) {
+        ArrayList<String> ar = new ArrayList<String>();
+        //Convertendo para binário e depois string
+        String bloco = Integer.toBinaryString(entrada);
+        bloco = checarTamanho(bloco, 8);
+        //quebrando a String em duas partes
+        String parteL = bloco.substring(0, bloco.length() / 2);
+        String parteR = bloco.substring(bloco.length() / 2, bloco.length());
+        ar.add(parteL);
+        ar.add(parteR);
+        return ar;
+    }
+
+    public void funcao2() {
+
+    }
 }
--- src/test_criptografia_seg_redes_lab1_cifra_feistel/Test_Criptografia_Seg_Redes_lab1_Cifra_Feistel.java
+++ src/test_criptografia_seg_redes_lab1_cifra_feistel/Test_Criptografia_Seg_Redes_lab1_Cifra_Feistel.java
@@ -17,31 +17,43 @@ public class Test_Criptografia_Seg_Redes_lab1_Cifra_Feistel {
      */
     public static void main(String[] args) {  
         Scanner sc = new Scanner(System.in);
-        System.out.println("Insira um número decimal de 8 bits:");
-        int valor = sc.nextInt();
-        System.out.println("Insira uma chave decimal de 4 bits:");
-        int key1 = sc.nextInt();
-        System.out.println("Insira outra chave decimal de 4 bits:");
-        int key2 = sc.nextInt();
+        //System.out.println("Insira um número decimal de 8 bits:");
+        //int valor = sc.nextInt();
+        System.out.println("Insira o texto a ser criptografado:");
+        String valor = sc.nextLine();
+        
+        System.out.println("Insira a primeira chave:");
+        //int key1 = sc.nextInt();
+        String key1 = sc.nextLine();
+        System.out.println("Insira a segunda chave:");
+        //int key2 = sc.nextInt();
+        String key2 = sc.nextLine();
         
         //processo de criptografia
         CifraFeistel cf = new CifraFeistel();                
-        String bloco = cf.criptografar(valor,key1,key2);
+        //String bloco = cf.criptografar(valor,key1,key2);
+        //String bloco = cf.realizarCriptografia(valor, key1, key2);
+        String keyAux1 = String.valueOf(key1);
+        String keyAux2 = String.valueOf(key2);
+        //String bloco = cf.realizarCriptografia(valor, String.valueOf(key1), String.valueOf(key2));
+        String bloco = cf.realizarCriptografia(valor, keyAux1, keyAux2);
 
         Binario b = new Binario();
         
-        System.out.println("Número inserido: "+valor);
-        System.out.println("Caractere associado: "+(char)valor);
+        //System.out.println("Número inserido: "+valor);
+        //System.out.println("Caractere associado: "+(char)valor);
                 
-        int blocoCripto = b.converterBinarioParaDecimal(bloco);
-        System.out.println("Criptografado: "+blocoCripto);
-        int a = blocoCripto;
-        System.out.println("Caractere associado - criptografia: "+(char)a);
+        //int blocoCripto = b.converterBinarioParaDecimal(bloco);
+        //System.out.println("Criptografado: "+blocoCripto);
+        System.out.println("Criptografado: "+bloco);
+        System.out.println("Descriptografado: "+cf.realizarDescriptografia(bloco, keyAux1, keyAux2));
+        //int a = blocoCripto;
+        //System.out.println("Caractere associado - criptografia: "+(char)a);
         
         //processo de descriptografia
-        bloco = cf.descriptografar(blocoCripto,key1,key2);
-        System.out.println("Descriptografado: "+b.converterBinarioParaDecimal(bloco)); 
-        a = b.converterBinarioParaDecimal(bloco);
-        System.out.println("Caracter associado - descriptografado: "+(char)a);
+        //bloco = cf.descriptografar(blocoCripto,key1,key2);
+        //System.out.println("Descriptografado: "+b.converterBinarioParaDecimal(bloco)); 
+        //a = b.converterBinarioParaDecimal(bloco);
+        //System.out.println("Caracter associado - descriptografado: "+(char)a);
     }   
 }
